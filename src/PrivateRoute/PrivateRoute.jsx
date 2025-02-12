c6087ff309de80bb3685706e1ac5f0407653b025
tushar54 <tusharahmed7083@gmail.com>
2025-02-12 17:33:02 +0600

    i have solved some loading releted bugs

--- src/PrivateRoute/PrivateRoute.jsx
+++ src/PrivateRoute/PrivateRoute.jsx
@@ -4,16 +4,8 @@ import { Context } from "../AllContext/Authcontext";
 
 
 const PrivateRoute = ({ children }) => {
-    const { currentUser, loading } = useContext(Context)
+    const { currentUser, loading,setLoading } = useContext(Context)
     const location = useLocation();
-
-
-    if (loading) {
-        return <div className="fixed inset-0 flex items-center justify-center bg-gray-100">
-            <span className="loading loading-dots loading-lg"></span>
-        </div>
-    }
-
     if (currentUser && currentUser?.email) {
         return children;
     }
